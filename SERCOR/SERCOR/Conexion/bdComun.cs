693df1422119a7489d598416f00518ff1948be51
Ernesteins <eyaselga@hotmail.com>
2018-11-18 17:39:30 -0500

    Inicio + conexion

---
+++ SERCOR/SERCOR/Conexion/bdComun.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SERCOR.Conexion
+{
+    class bdComun
+    {
+        //CONEXION
+        public static MySqlConnection obtenerConexion()
+        {
+            //Variables de conexion
+            string server = "127.0.0.1";
+            string databaseName = "sercorDB";
+            string user = "sercoruser";
+            string password = "f9f524bf";
+
+            MySqlConnection conectar = new MySqlConnection("server=" + server + ";" +
+            "database=" + databaseName + ";" +
+            "Uid=" + user + ";" +
+            "pwd=" + password + ";" +
+            "SslMode=none;");
+            conectar.Open();
+            return conectar;
+           
+        }
+    }
+}
